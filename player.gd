a62ddd27fc41aa148bb35eed224b55acb66d22d2
Thonny Do <thongd09@gmail.com>
2026-02-05 20:47:41 -0500

    extras

--- player.gd
+++ player.gd
@@ -8,7 +8,12 @@ var x_direction: int = 0
 var alive: bool = true
 var double_jump: bool = true
 var unlock_dl: bool = false
+var can_dash: bool = true
+var unlock_dsh: bool = false
 var runtimer = true
+var dashspd = [0,0] #speed, direction
+var DASHMULT: int = 500 #how fast you dash
+var lastdir = 0 #stores either 1 or -1 for your last direction moved
 @export var cam: Camera2D
 @export var win: Node2D
 
@@ -21,30 +26,40 @@ func _process(_delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
+	if dashspd[0] > 0:
+		dashspd[0] -= 10
+		print(dashspd)
+	else: dashspd[0] = 0
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if runtimer:
 			$coyotetime.start() # Restart it
 			runtimer = false
-	if is_on_floor(): runtimer = true
+	if is_on_floor(): 
+		runtimer = true
+		can_dash = true
 	if alive:
 		if is_on_floor() and !double_jump and unlock_dl:
 			double_jump = true
 		if Input.is_action_pressed("go_left"):
 			x_direction = -1
+			lastdir = -1
 			$AnimatedSprite2D.flip_h = true
 		elif Input.is_action_pressed("go_right"):
 			x_direction = 1
+			lastdir = 1
 			$AnimatedSprite2D.flip_h = false
 		else:
 			x_direction = 0
-		velocity.x = x_direction * SPEED
 		
 		if Input.is_action_just_pressed("go_jump") and (is_on_floor() or !$coyotetime.is_stopped()):
 			velocity.y = JUMP_POWER
 		elif Input.is_action_just_pressed("go_jump") and !is_on_floor() and double_jump and unlock_dl:
 			velocity.y = JUMP_POWER
 			double_jump = false
+		
+		if Input.is_action_just_pressed("go_dash") and can_dash and unlock_dsh:
+			dashspd = [DASHMULT, lastdir]
 			
 		if is_on_floor():
 			if x_direction != 0:
@@ -70,10 +85,15 @@ func _physics_process(delta: float) -> void:
 			else:
 				new_disc.x_direction = 1
 			get_tree().current_scene.add_child(new_disc) # Add it to the scene
+			
+		velocity.x = (x_direction * SPEED) + (dashspd[0] * dashspd[1])
 	move_and_slide()
 
 func unlocked():
 	unlock_dl = true
+
+func dashlocked():
+	unlock_dsh = true
 
 func die():
 	cam.position_smoothing_speed = DEATHPAN
@@ -114,3 +134,20 @@ func youwin():
 	Engine.time_scale = 0.2
 	await get_tree().create_timer(DEATH_DELAY*1.5).timeout
 	Engine.time_scale = 1.0
+
+func _on_tickrate_timeout() -> void: #for dash effects
+	if dashspd[0] > 0:
+		var cloned_node: Node = $AnimatedSprite2D.duplicate()
+		#print("i clone")
+		cloned_node.name = "mirage"
+		get_parent().add_child(cloned_node)
+		cloned_node.global_position = global_position
+		cloned_node.z_index = -2
+		var c: Color = cloned_node.modulate
+		c.a = float(dashspd[0])/float(DASHMULT)
+		cloned_node.modulate = c
+		#print(global_position, cloned_node.global_position)
+		var tween = get_tree().create_tween()
+		tween.tween_property(cloned_node, "modulate:a", 0.0, 0.5)
+		tween.tween_callback(cloned_node.queue_free)
+		
